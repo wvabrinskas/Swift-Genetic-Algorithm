0529446d0bd3bb325ee5af8e7a186e8d77b74606
William Vabrinskas <williamvabrinskas@Williams-MBP.home>
2018-03-30 10:14:46 -0400

    first commit

---
+++ GeneticAlgorithm/Genetic.swift
@@ -0,0 +1,9 @@
+//
+//  Genetic.swift
+//  GeneticAlgorithm
+//
+//  Created by William Vabrinskas on 3/29/18.
+//  Copyright © 2018 William Vabrinskas. All rights reserved.
+//
+
+import Foundation
